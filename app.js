08b56f6598ed885cab7f6f15a6c30a92d7b2dd50
gasht1 <gebriegashaw7@gmail.com>
2024-08-29 23:49:54 +0400

    after added mouseover/out

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-document.addEventListener("cliked", function () {
+/*document.addEventListener("click", function () {
   console.log(" you clicked document page");
+});
+document.addEventListener("mouseOver", function () {
+
+    const h1Element=document.querySelector("h1");
+});*/
+const header = document.querySelector("h1");
+const orginalText = header.textContent;
+const updatedText = "build an AR APP";
+header.addEventListener("mouseover", function () {
+  header.textContent = updatedText;
+});
+header.addEventListener("mouseout", function () {
+  header.textContent = orginalText;
 });
